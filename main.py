68ad1345f89d432ae4fa98e405bec8517fc5c797
zhenwufan <83192003+zhenwufan@users.noreply.github.com>
2022-08-19 22:20:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 bot_token = os.environ.get('BOT_TOKEN')
-workers = int(os.environ.get('WORKERS'))
-api = int(os.environ.get('API_KEY'))
+workers = os.environ.get('WORKERS')
+api = os.environ.get('API_KEY')
 hash = os.environ.get('API_HASH')
 chnnl = os.environ.get('CHANNEL_URL')
 BOT_URL = os.environ.get('BOT_URL')
